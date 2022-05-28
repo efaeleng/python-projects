7ccf825cb37429a3d51a647911b8f8ae0683edf5
Efa-iwa Eleng <gistend@gmail.com>
2022-05-28 19:44:56 +0100

    read text files

---
+++ Reading-Text-Files/Reading-Text-Files/main.py
@@ -0,0 +1,22 @@
+# Read text from a file, and count the occurence of words in that text
+# Example:
+# count_words("The cake is done. It is a big cake!") 
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+
+def read_file_content(filename):
+    # [assignment] Add your code here 
+    #    with open('./story.txt', 'r') as f:
+    #        return f.read()
+    with open('./story.txt') as file:
+     for line in file:
+         print(line)
+    return "Hello World"
+# read_file_content('story.txt')
+
+
+
+# def count_words():
+#     text = read_file_content("./story.txt")
+#     # [assignment] Add your code here
+
+#     return {"as": 10, "would": 20}
