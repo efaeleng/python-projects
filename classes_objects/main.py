dcb4f5cda012d5f7a6972ccb7684e8af326ad7bf
Efa-iwa Eleng <gistend@gmail.com>
2022-05-30 12:56:39 +0100

    fixed error

--- classes_objects/main.py
+++ classes_objects/main.py
@@ -1,35 +1,31 @@
 class Student:
     # [assignment] Skeleton class. Add your code here
-    student = "Bob"
+    def __init__(self, name, age, tracks, score): #
+        self.name = str(name) 
+        self.age = int(age)
+        self.tracks = list(tracks)
+        self.score = float(score)
+        print("The student's information is displayed below:")
 
-    def __init__(self):
-        pass
-        # self.name = "Bob"
+    def change_name(self, new_name):
+        self.name = new_name
+        print('name is :', self.name)
 
-    
+    def change_age(self, new_age):
+        self.age = new_age
+        print(self.name,"'s age is :", self.age)
 
-    def change_name(self, name):
-        self.name = name
-
-    def change_age(self, age):
-        self.age = age
-
-    def add_track(self, tracks):
-        self.tracks = tracks
-
-    def set_score(self, score):
-        self.score = score
+    def add_track(self, new_track):
+        self.tracks.append(new_track)
+        print(self.name,"'s new tracks : ", self.tracks)
 
     def get_score(self):
-        return self.score
-
+        print(self.name,"'s scores is :", self.score)
 
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
 # Expected methods
-print(Bob.change_name("Peter"))
-
-# Bob.change_age(34)
-# Bob.add_track("UI/UX")
-# Bob.set_score(20.90)
-# Bob.get_score()
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
